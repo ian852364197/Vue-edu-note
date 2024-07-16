61ae61efa870078fa7de2226fcfe06da65cc1cbf
IanTang <iantang@uitc.com.tw>
2024-07-16 15:17:46 +0800

    feat. 新增slot

---
+++ src/components/ChildButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>Slot</h2>
+  <button @click="showSlot = !showSlot">{{ btnMsg }}</button>
+  <div v-show="showSlot">
+    <p>parent message被固定了</p>
+    <p>model message隨emit次數改變</p>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+const showSlot = ref(false);
+const btnMsg = computed(() => {
+  return showSlot.value ? 'hide slot' : 'show slot';
+});
+</script>
+
+<style scoped></style>
--- src/views/CompView.vue
+++ src/views/CompView.vue
@@ -2,19 +2,28 @@
   <div>
     <h1>Parent View</h1>
     <input v-model="parentMsg" placeholder="parent message" />
-    <p>emit times: {{ emitTimes }}</p>
+    <p>Child View emit times: {{ emitTimes }}</p>
     <input v-model="msgModel" placeholder="model message" />
     <ChildView :pMsg="parentMsg" @childInput="emitTimes++" v-model="msgModel"></ChildView>
+    <p>Slot emit times: {{ slotCount }}</p>
+    <ChildButtom
+      ><ChildView pMsg="component in slot" @childInput="slotTimes++" v-model="slotTimes"></ChildView
+    ></ChildButtom>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import ChildView from '@/components/ChildComp.vue';
+import ChildButtom from '@/components/ChildButton.vue';
 
 const parentMsg = ref('');
 const emitTimes = ref(0);
 const msgModel = ref('');
+const slotTimes = ref(0);
+const slotCount = computed(() => {
+  return !slotTimes.value ? 0 : slotTimes;
+});
 </script>
 
 <style scoped></style>
