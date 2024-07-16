72de512bca4761b2dba943dd3992c7222baf4b67
IanTang <iantang@uitc.com.tw>
2024-07-16 14:17:20 +0800

    feat. 新增emit

--- src/components/ChildComp.vue
+++ src/components/ChildComp.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <h2>Child View</h2>
-    <p>{{ props.pMsg }}</p>
+    <p>parent message: {{ props.pMsg || 'No message' }}</p>
+    <button @click="emit('childInput')">emit</button>
   </div>
 </template>
 
 <script setup>
 const props = defineProps(['pMsg']);
+const emit = defineEmits(['childInput']);
 </script>
 
 <style scoped></style>
--- src/views/CompView.vue
+++ src/views/CompView.vue
@@ -2,7 +2,8 @@
   <div>
     <h1>Parent View</h1>
     <input v-model="parentMsg" placeholder="parent message" />
-    <ChildView :pMsg="parentMsg"></ChildView>
+    <p>emit times: {{ emitTimes }}</p>
+    <ChildView :pMsg="parentMsg" @childInput="emitTimes++"></ChildView>
   </div>
 </template>
 
@@ -11,6 +12,7 @@ import { ref } from 'vue';
 import ChildView from '@/components/ChildComp.vue';
 
 const parentMsg = ref('');
+const emitTimes = ref(0);
 </script>
 
 <style scoped></style>
