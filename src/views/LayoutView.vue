a9c22a87ad2f217e61d078db90194aa3be35fe47
IanTang <iantang@uitc.com.tw>
2024-07-23 08:43:49 +0800

    feat. 移除套模板前的js程式

--- src/views/LayoutView.vue
+++ src/views/LayoutView.vue
@@ -2,7 +2,7 @@
   <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <button class="btn navbar-brand" @click="destination = ''">
+        <button class="btn navbar-brand">
           <RouterLink to="/">Home</RouterLink>
         </button>
         <button
@@ -27,47 +27,15 @@
         </div>
       </div>
     </nav>
-    <!-- <div>
-      <nav>
-        <ul>
-          <li>
-            <button @click="destination = ''"><RouterLink to="/">Home</RouterLink></button>
-          </li>
-          <li>
-            <select v-model="destination">
-              <option v-for="pageName in pages" :key="pageName" :value="pageName">
-                {{ pageName }}
-              </option>
-            </select>
-          </li>
-          <li v-for="pageName in pages" :key="pageName">
-            <RouterLink :to="pageName">{{ pageName }}</RouterLink>
-          </li>
-        </ul>
-      </nav>
-    </div> -->
   </header>
   <RouterView />
 </template>
 
 <script setup>
-import { RouterLink, RouterView, useRouter } from 'vue-router';
-import { ref, watch } from 'vue';
-
-const router = useRouter();
-
-const destination = ref('');
+import { RouterLink, RouterView } from 'vue-router';
+import { ref } from 'vue';
 
 const pages = ref(['ref', 'bind', 'event', 'vfor', 'computed', 'component']);
-
-watch(
-  () => destination.value,
-  () => {
-    if (destination.value) {
-      router.push(destination.value);
-    }
-  }
-);
 </script>
 
 <style scoped></style>
