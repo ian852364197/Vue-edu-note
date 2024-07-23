c96276e68649683b127290670287a9f6a1f40cec
IanTang <iantang@uitc.com.tw>
2024-07-23 08:41:34 +0800

    feat. layout套上bootstrap

--- src/views/LayoutView.vue
+++ src/views/LayoutView.vue
@@ -1,6 +1,33 @@
 <template>
   <header>
-    <div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <button class="btn navbar-brand" @click="destination = ''">
+          <RouterLink to="/">Home</RouterLink>
+        </button>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item" v-for="pageName in pages" :key="pageName">
+              <button class="btn nav-item">
+                <RouterLink :to="pageName">{{ pageName }}</RouterLink>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- <div>
       <nav>
         <ul>
           <li>
@@ -18,7 +45,7 @@
           </li>
         </ul>
       </nav>
-    </div>
+    </div> -->
   </header>
   <RouterView />
 </template>
